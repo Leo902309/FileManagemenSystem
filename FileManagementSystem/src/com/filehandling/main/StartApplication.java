5bf26a707b4589c4353ca4f95cd34ebad7c506c9
brajeshkumar <brajeshkumar@brajesh>
2021-01-18 15:08:35 +0530

    JUst checking

--- FileManagementSystem/src/com/filehandling/main/StartApplication.java
+++ FileManagementSystem/src/com/filehandling/main/StartApplication.java
@@ -5,12 +5,12 @@ import java.util.Scanner;
 public class StartApplication {
 	 public static void main(String[] args) 
 	    {
+		 System.out.println("Just for commit purpose...");
 		 Service service=new Service();
 	     Scanner scan = new Scanner(System.in);
 		 while(true) {
 			 service.menuList(scan);
 		 }
-
 	    }
 	
 }
