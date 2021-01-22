f9fd838075e48af32fe95463daa42c603df2ae65
brajeshkumar <brajeshkumar@brajesh>
2021-01-23 00:02:21 +0530

    commiting in hostfix branch

--- FileManagementSystem/src/com/filehandling/main/StartApplication.java
+++ FileManagementSystem/src/com/filehandling/main/StartApplication.java
@@ -5,9 +5,7 @@ import java.util.Scanner;
 public class StartApplication {
 	 public static void main(String[] args) 
 	    {
-		 System.out.println("doining a commit 1 in master branch");
-		 System.out.println("Creating commit 2 in master branch");
-		 System.out.println("Making commit 1 in hotfix branch");
+		  System.out.println("Hot fix selcted...");
 		 Service service=new Service();
 	     Scanner scan = new Scanner(System.in);
 		 while(true) {
